9772c044f02046462f956806145c3c07eecc4764
Olivia Ejde <ozejde@users.noreply.github.com>
2016-10-28 01:49:43 +0000

    Switched status report to correct location

--- ArcadeGameProject/src/Main.java
+++ ArcadeGameProject/src/Main.java
@@ -3,8 +3,8 @@ import javax.swing.JFrame;
 /**
  * The main class for your arcade game.
  * 
- * You can design your game any way you like, but make the game start
- * by running main here.
+ * You can design your game any way you like, but make the game start by running
+ * main here.
  * 
  * Also don't forget to write javadocs for your classes and functions!
  * 
@@ -16,15 +16,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("BomberMan Test");
-		
-		DrawPanel panel = new DrawPanel();		
-		
+
+		DrawPanel panel = new DrawPanel();
+
 		frame.add(panel);
 		frame.setSize(834, 688);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setResizable(false);
-		
+
 	}
 
 }
