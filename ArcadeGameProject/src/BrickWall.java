d55932840e3defc621d1f7671b6216567a829851
ejdeoz <ejdeoz@702d94d4-ebfc-4664-b33a-6edb8ec05c6b>
2016-10-28 02:19:26 +0000

    JavaDocs completed for brickwall and bomb

--- ArcadeGameProject/src/BrickWall.java
+++ ArcadeGameProject/src/BrickWall.java
@@ -1,10 +1,10 @@
 import java.awt.image.BufferedImage;
+
 /**
  * 
  * Wall object that can be removed by Bomb
  *
- * @author ejdeoz.
- *         Created Oct 27, 2016.
+ * @author ejdeoz, youngqom, petersmt. Created Oct 27, 2016.
  */
 public class BrickWall extends Tile {
 
@@ -13,17 +13,21 @@ public class BrickWall extends Tile {
 	 * Creates BrickWall object
 	 *
 	 * @param x1
-	 * x-coordinate at 
+	 *            x-coordinate at 
 	 * @param y1
-	 * 
+	 *            y-coordinate at
 	 * @param x2
-	 * 
+	 *            x-coordinate at 
 	 * @param y2
-	 * 
+	 *            y-coordinate at
 	 * @param tileSheet
+	 *            File being read from
 	 * @param x
+	 * 
 	 * @param y
+	 * 
 	 */
+
 	public BrickWall(int x1, int y1, int x2, int y2, BufferedImage tileSheet, int x, int y) {
 		super(x1, y1, x2, y2, tileSheet, x, y);
 		super.setDistructable(true);
