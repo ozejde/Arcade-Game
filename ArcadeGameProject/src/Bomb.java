0884e059ca1ba0a4f4f45c5e0e46558aae521dea
ejdeoz <ejdeoz@702d94d4-ebfc-4664-b33a-6edb8ec05c6b>
2016-10-28 02:08:10 +0000

    bomb now goes under hero

--- ArcadeGameProject/src/Bomb.java
+++ ArcadeGameProject/src/Bomb.java
@@ -3,18 +3,41 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
-public class Bomb{
+/**
+ * 
+ * A black, circular graphic that can destroy BrickWall
+ *
+ * @author ejdeoz, youngqom, petersmt. Created Oct 27, 2016.
+ */
+public class Bomb {
 	private int x;
 	private int y;
-	
+
+	/**
+	 * 
+	 * Sets the coordinates of Bomb
+	 *
+	 * @param x
+	 *            x-coordinate
+	 * @param y
+	 *            y-coordinate
+	 */
 	public Bomb(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * 
+	 * Draws image of Bomb
+	 * 
+	 * @param graphics
+	 * Graphics that Bomb is painted onto
+	 * 
+	 */
 	public void drawCharacter(Graphics graphics) {
 		graphics.setColor(Color.BLACK);
-		Graphics2D gCast  = (Graphics2D) graphics;
+		Graphics2D gCast = (Graphics2D) graphics;
 		Ellipse2D.Double bomb = new Ellipse2D.Double(this.x, this.y, 20, 20);
 		gCast.fill(bomb);
 	}
--- ArcadeGameProject/src/BrickWall.java
+++ ArcadeGameProject/src/BrickWall.java
@@ -1,7 +1,29 @@
 import java.awt.image.BufferedImage;
-
+/**
+ * 
+ * Wall object that can be removed by Bomb
+ *
+ * @author ejdeoz.
+ *         Created Oct 27, 2016.
+ */
 public class BrickWall extends Tile {
 
+	/**
+	 * 
+	 * Creates BrickWall object
+	 *
+	 * @param x1
+	 * x-coordinate at 
+	 * @param y1
+	 * 
+	 * @param x2
+	 * 
+	 * @param y2
+	 * 
+	 * @param tileSheet
+	 * @param x
+	 * @param y
+	 */
 	public BrickWall(int x1, int y1, int x2, int y2, BufferedImage tileSheet, int x, int y) {
 		super(x1, y1, x2, y2, tileSheet, x, y);
 		super.setDistructable(true);
