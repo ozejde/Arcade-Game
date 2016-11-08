2d0b9b2c02c05e22ad4a4471fbe3cecddb991ea4
ejdeoz <ejdeoz@702d94d4-ebfc-4664-b33a-6edb8ec05c6b>
2016-11-08 18:54:28 +0000

    Frame closes on start

--- ArcadeGameProject/src/GameSetupListener.java
+++ ArcadeGameProject/src/GameSetupListener.java
@@ -1,16 +1,22 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
 public class GameSetupListener implements ActionListener{
 	private Main main;
+	private JFrame frame;
 
-	public GameSetupListener(Main main) {
+	public GameSetupListener(Main main, JFrame frame) {
 		this.main = main;
+		this.frame = frame;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		frame.dispose();
 		this.main.gameSetup();
+		
 	}
 
 }
