8cf1dd1fb952400fd7f2595ba59534328055014b
ejdeoz <ejdeoz@702d94d4-ebfc-4664-b33a-6edb8ec05c6b>
2016-11-08 23:56:13 +0000

    Added buttons to the game screen and centered the lives counter

--- ArcadeGameProject/src/Main.java
+++ ArcadeGameProject/src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BoxLayout;
@@ -21,20 +22,19 @@ import javax.swing.JPanel;
  */
 
 public class Main {
-	
 
 	public static void main(String[] args) {
 		JButton startButton = new JButton("Start Game");
 		JButton instrButton = new JButton("Instructions");
-		
+
 		JPanel p = new JPanel();
 		p.add(startButton);
 		p.add(instrButton);
-		
+
 		JFrame frame = new JFrame("Start Game");
 		frame.add(p, BorderLayout.SOUTH);
 		frame.add(new JLabel(new ImageIcon("titlepic.png")), BorderLayout.NORTH);
-		
+
 		Main main = new Main();
 		startButton.addActionListener(new GameSetupListener(main, frame));
 		instrButton.addActionListener(new GameSetupListener(main, frame));
@@ -44,19 +44,19 @@ public class Main {
 		frame.setResizable(false);
 
 	}
-	
+
 	public void startSetup() {
 		JButton startButton = new JButton("Start Game");
 		JButton instrButton = new JButton("Instructions");
-		
+
 		JPanel p = new JPanel();
 		p.add(startButton);
 		p.add(instrButton);
-		
+
 		JFrame frame = new JFrame("Start Game");
 		frame.add(p, BorderLayout.SOUTH);
 		frame.add(new JLabel(new ImageIcon("titlepic.png")), BorderLayout.NORTH);
-		
+
 		Main main = new Main();
 		startButton.addActionListener(new GameSetupListener(main, frame));
 		instrButton.addActionListener(new GameSetupListener(main, frame));
@@ -64,16 +64,16 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setResizable(false);
-		
+
 	}
 
-	public void instuctSetup(){
+	public void instuctSetup() {
 		JFrame frame = new JFrame();
 		frame.setTitle("Bomber Man Instructions");
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		
-		JButton backButton = new JButton("Back");
+
+		JButton backButton = new JButton("Back to Menu");
 		JButton startButton = new JButton("Start Game");
 		backButton.addActionListener(new GameSetupListener(this, frame));
 		startButton.addActionListener(new GameSetupListener(this, frame));
@@ -81,56 +81,61 @@ public class Main {
 		buttonPanel.add(backButton);
 		buttonPanel.add(startButton);
 		frame.add(buttonPanel, BorderLayout.SOUTH);
-		
-		
+
 		JLabel titleLabel = new JLabel("Instructions");
 		titleLabel.setFont(new Font("Sans Serif", Font.PLAIN, 40));
 		panel.add(titleLabel);
-		
-		JLabel manualLabel = new JLabel("<html>Controls"
-				+" <br> • Press the arrow keys to move"
-				+ "<br> • Press B to drop bombs"
-				+ "<br> • Press U to play the next level, and D to play the previous level"
-				+ "<br> • Press Z to detonate controllable bombs (Power-up ability) "
-				+ "<br> "
-				+ "<br> General Rules"
-				+ "<br> • You cannot pass through brick or concrete walls"
-				+ "<br> • Bombs will kill monsters, destroy brick walls, and damage you"
-				+ "<br> • Some brick walls contain powerups beneath them, only accessable when the wall is bombed"
-				+ "<br> • The Bomb's reach is one tile in each cardinal direction (North, South, East, West)"
-				+ "<br> • If you are within range of the bomb, you will be damaged and lose one life"
-				+ "<br> • Only one bomb can be on the screen at one time, except if a powerup is being used"
-				+ "<br> • If multiple bombs are on the screen, bombs will explode if in the range of another exploding bomb"
-				+ "<br> • Coming into contact with a monster will cause you to lose one life"
-				+ "<br> • Your remaining number of lives can be seen in the upper left corner of the screen"
-				+ "<br> • You can only gain lives through certain powerups"
-				+ "<br>"
-				+ "<br>Power-Ups"
-				+ "<br> • Purple tiles add a life"
-				+ "<br> • Red tiles make the bombs detonatable"
-				+ "<br> • Orange tiles increase the bomb's blast distance by one tile in all directions"
-				+ "<br> • Blue allows you to drop more than one bomb at a time"
-				+ "<br> • Death removes all power-ups "
-				+ "<br> • Leveling up removes all power ups<html>");
-		
+
+		JLabel manualLabel = new JLabel(
+				"<html>Controls" + " <br> • Press the arrow keys to move" + "<br> • Press B to drop bombs"
+						+ "<br> • Press U to play the next level, and D to play the previous level"
+						+ "<br> • Press Z to detonate controllable bombs (Power-up ability) " + "<br> "
+						+ "<br> General Rules" + "<br> • You cannot pass through brick or concrete walls"
+						+ "<br> • Bombs will kill monsters, destroy brick walls, and damage you"
+						+ "<br> • Some brick walls contain powerups beneath them, only accessable when the wall is bombed"
+						+ "<br> • The Bomb's reach is one tile in each cardinal direction (North, South, East, West)"
+						+ "<br> • If you are within range of the bomb, you will be damaged and lose one life"
+						+ "<br> • Only one bomb can be on the screen at one time, except if a powerup is being used"
+						+ "<br> • If multiple bombs are on the screen, bombs will explode if in the range of another exploding bomb"
+						+ "<br> • Coming into contact with a monster will cause you to lose one life"
+						+ "<br> • Your remaining number of lives can be seen in the upper left corner of the screen"
+						+ "<br> • You can only gain lives through certain powerups" + "<br>" + "<br>Power-Ups"
+						+ "<br> • Purple tiles add a life" + "<br> • Red tiles make the bombs detonatable"
+						+ "<br> • Orange tiles increase the bomb's blast distance by one tile in all directions"
+						+ "<br> • Blue allows you to drop more than one bomb at a time"
+						+ "<br> • Death removes all power-ups " + "<br> • Leveling up removes all power ups<html>");
+
 		manualLabel.setFont(new Font("Sans Serif", Font.PLAIN, 20));
 		panel.add(manualLabel);
-		
+
 		frame.add(panel);
 		frame.setSize(834, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
-	
-	public void gameSetup(){
+
+	public void gameSetup() {
 		JFrame frame = new JFrame();
 		DrawPanel panel = new DrawPanel();
+		JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 0));
+
+		JButton backButton = new JButton("Back to Menu");
+		backButton.addActionListener(new GameSetupListener(this, frame));
+		labelPanel.add(backButton);
+
 		JLabel label = panel.label;
 		label.setText("Lives: " + panel.hero.getLives());
 		label.setFont(new Font("Sans Serif", Font.BOLD, 30));
+		labelPanel.add(label);
+		frame.add(labelPanel, BorderLayout.NORTH);
+
+		JButton instrButton = new JButton("Instructions");
+		instrButton.addActionListener(new GameSetupListener(this, frame));
+		labelPanel.add(instrButton);
+
 		frame.setTitle("BomberMan Game");
-		frame.add(label, BorderLayout.NORTH);
+
 		frame.add(panel);
 		frame.setSize(834, 688);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
